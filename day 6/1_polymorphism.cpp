282e1609de0f259a5b1dc22ef0027b92116e9503
hallikar01 <naveenkunchitiga@gmail.com>
2025-05-28 09:45:03 +0530

    updated code

---
+++ day 6/1_polymorphism.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=20;
+    cout<<"sum:"<<a+b<<endl;
+    string s1="HELLO";
+    string s2="WORLD";
+cout<<"contact:"<<s1+s2<<endl;
+return 0;
+}
---
+++ day 6/3_notification.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+class massage
+{
+public:
+void sendmessage(string email, string message)
+{
+    cout <<"email sent successfully:"endl;
+}
+}
